85094174fd7dc74306a25add94821381e2712b20
skimby <sallykim13@gmail.com>
2022-11-28 12:56:23 -0600

    d3

---
+++ assets/js/d3.js
@@ -0,0 +1,11 @@
+// d3.select(); //selects first element that meets critera
+// d3.selectAll(); //selects all elements that meet criteria
+
+// d3.select('h1').style('color', 'red')
+//     .attr('class', 'head')
+//     .text('updated h1 tag')
+
+
+//data
+
+const data = [1, 2, 3, 4, 5];
